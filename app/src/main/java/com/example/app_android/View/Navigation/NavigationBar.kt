3d1c4370e8f04ee313d59506093582af53f55d62
Joel <joelrt1016@gmail.com>
2025-02-15 10:34:02 +0100

    Creacion del nav

---
+++ app/src/main/java/com/example/app_android/View/Navigation/NavigationBar.kt
@@ -0,0 +1,47 @@
+package com.example.app_android.View.Navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+sealed class BottomNavItem(val route: String, val icon: ImageVector, val title: String) {
+    object Perfil : BottomNavItem("perfil", Icons.Default.Person, "Perfil")
+    object Reservas : BottomNavItem("reservas", Icons.Default.DateRange, "Reservas")
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Perfil,
+        BottomNavItem.Reservas
+    )
+
+    NavigationBar {
+        val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    if (currentRoute != item.route) {
+                        navController.navigate(item.route)
+                    }
+                }
+            )
+        }
+    }
+}
+
